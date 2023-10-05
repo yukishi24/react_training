fa05b90a3a532cea29717e0343926b8da78eea72
yukishi24 <yukishi24github@gmail.com>
2023-10-05 23:11:53 +0900

    modify: vue-cli project

---
+++ sample_app/vue.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  }
+})
+module.exports = {
+  devServer: {
+    port: 4500,
+  },
+
+  pluginOptions: {
+    vuetify: {}
+  }
+}
