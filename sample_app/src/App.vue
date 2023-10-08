c9b63201f626b6e7817068c6b19c45503c3ce409
yukishi24 <yukishi24github@gmail.com>
2023-10-08 23:25:45 +0900

    add: link logic

--- sample_app/src/App.vue
+++ sample_app/src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <v-app>
+    <v-app-bar>
+      <v-app-bar-title>
+        <router-link to="/sample">hello</router-link>
+      </v-app-bar-title>
+    </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
   </v-app>
+
 </template>
 
 <script>
